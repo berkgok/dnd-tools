2eb757dbe3144617eeff5b9a33463860b31751e4
berkgok <berkgokwork@gmail.com>
2019-09-19 14:37:37 +0300

    Initial commit

---
+++ src/main/java/com/dnd/dndtools/DndToolsApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.dndtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndToolsApplication.class, args);
+    }
+
+}
